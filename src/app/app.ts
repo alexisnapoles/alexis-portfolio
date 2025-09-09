646997ec57fbba225b644976ff8e10fbd52cca65
Alexis Dela Torre <alexisndelatorre@gmail.com>
2025-09-10 00:06:36 +0800

    modified: (features) : [layout, spacing, and sizing]

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +9,7 @@ import { RouterLink } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('portfolio');
+
+  protected readonly now = signal(new Date());
+  year = computed(() => this.now().getFullYear());
 }
