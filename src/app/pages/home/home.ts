bb0ad60cca874fb9de3b08e551ece81ebe75dbe4
Alexis Dela Torre <alexisndelatorre@gmail.com>
2025-09-10 22:55:03 +0800

    add: (component, content & features): about

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroBanner } from '../../shared/components/hero-banner/hero-banner';
+import { About } from '../about/about';
+About;
 
 @Component({
   selector: 'app-home',
-  imports: [HeroBanner],
+  imports: [HeroBanner, About],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
